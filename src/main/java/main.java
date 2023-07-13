21da08a3d0b4d67f9f0b11740dc900dbd939c414
taha <taha@kali.taha.com>
2023-07-13 09:00:01 +0100

    second commit, not full program

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class main {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.Set;
+
+
+import java.sql.Time;
+import java.time.Duration;
+
+public class main {
+
+    public static void main(String[] args){
+
+        FirefoxDriver driver = new FirefoxDriver();
+
+        driver.get("insert url here");
+
+        driver.findElement(By.cssSelector("insert button id here")).click();
+
+
+        try{
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e){
+
+            e.printStackTrace();
+        }
+
+        driver.findElement(By.cssSelector("insert button id here")).click();
+
+        String parentWindow = driver.getWindowHandle(); // Get the handle of the parent window
+        Set<String> windowHandles = driver.getWindowHandles(); // Get all available window handles
+
+
+
+
+        for (String windowHandle : windowHandles) {
+            if (!windowHandle.equals(parentWindow)) {
+
+                driver.switchTo().window(windowHandle);
+
+                try{
+                    Thread.sleep(2500);
+                }
+                catch (InterruptedException e){
+
+                    e.printStackTrace();
+                }
+                driver.findElement(By.id("enter id here")).sendKeys("enter email here");
+                break;
+            }
+        }
+
+
+    }
 }
+
+
+
+
